26bbbed51bfd9bc0a799794a36b1bd92b1348784
samBamkole <olagoroyesamuel7@gmail.com>
2025-02-13 13:33:50 +0100

    Corrected Groom's comment

--- src/components/StorySectionComponent.vue
+++ src/components/StorySectionComponent.vue
@@ -27,22 +27,30 @@ import shoot from "/photoshoot-2.jpg";
                     getting married.
                 </p>
                 <h4 class="font-semibold text-2xl mb-3 mt-2">Why Marriage?</h4>
-                <p>
+                <div>
                     After some time of being friends, we discovered that our values aligned in so
                     many ways and this led to the establishment of the group that prays and studies
-                    and now Inspirational Insight, and according to <b>The Groom</b>,
-                    <span class="block bg-gray-50 border-l-4 border-l-gray-400 py-2 px-2 my-2"
-                        >"what else can you be looking for in a woman, you found a woman who is
-                        interested in what you are interested in, she's beautiful and intelligent". </span
-                    >Then, we decided to take our relationship from friendship to marriage.
-                </p>
+                    and now Inspirational Insight, and according to:,
+                    <p class="block bg-gray-50 border-l-4 border-l-gray-400 py-2 px-2 my-2">
+                        "What more could you ask for in a partner? You have found a woman who shares
+                        your passions, is stunningly beautiful and remarkably intelligent."
+                        <span class="text-right italic block mt-2 font-semibold">- The Groom</span>
+                    </p>
+                    <p class="block bg-gray-50 border-r-4 border-r-gray-400 py-2 px-2 mb-2">
+                        "Enitan is a good man, he is Godly, loving and always willing to see the
+                        best in me. I know i have made the best decision about marriage and i'm
+                        grateful to God for making us meet at an early stage."
+                        <span class="text-right italic block mt-2 font-semibold">- The Bride</span>
+                    </p>
+                    Then, we decided to take our relationship from friendship to marriage.
+                </div>
             </div>
         </div>
-        <p class="mt-3 rounded-md p-5 gradient">
+        <!-- <p class="mt-3 rounded-md p-5 gradient">
             Enitan is a good man, he is Godly, loving and always willing to see the best in me. I
             know i have made the best decision about marriage and i'm grateful to God for making us
             meet at an early stage.
             <span class="text-right italic block mt-2 font-semibold">- The Bride</span>
-        </p>
+        </p> -->
     </section>
 </template>
